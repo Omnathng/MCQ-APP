09d4c5848de6a178cbfbc4919d21e827be844d06
Omnadhng <omnadhkannan@gmail.com>
2023-11-04 21:44:27 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,89 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
+import "./App.css";
+import { questions } from "./data/questions";
+import backgroundImage from "./assests/coding.gif";
 
 function App() {
+  const [showResults, setShowResults] = useState(false);
+  const [currentQuestion, setCurrentQuestion] = useState(0);
+  const [score, setScore] = useState(0);
+  const [userAnswerIndex, setUserAnswerIndex] = useState(null);
+
+  const optionClicked = (isCorrect, index) => {
+    if (userAnswerIndex === null) {
+      setUserAnswerIndex(index);
+      if (isCorrect) {
+        setScore(score + 1);
+      }
+    }
+    setTimeout(() => {
+      if (currentQuestion + 1 < questions.length) {
+        setCurrentQuestion(currentQuestion + 1);
+        setUserAnswerIndex(null);
+      } else {
+        setShowResults(true);
+      }
+    }, 500);
+  };
+
+  const restartGame = () => {
+    setScore(0);
+    setCurrentQuestion(0);
+    setShowResults(false);
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div
+      className="app d-flex align-items-center justify-content-center flex-column"
+      style={{
+        backgroundImage: `url(${backgroundImage})`,
+        height: "100vh",
+        backgroundSize: "cover",
+        backgroundPosition: "center",
+      }}
+    >
+      <h1 className="text-light fw-bolder">React Quiz</h1>
+      <h2 className="text-light fw-bolder">Score: {score}</h2>
+
+      {showResults ? (
+        <div className="final-results">
+          <h1>Final Score</h1>
+          <h2>
+            {score} out of {questions.length} correct - Mark is{" "}
+            {((score / questions.length) * 100).toFixed(2)}%
+          </h2>
+          <button onClick={() => restartGame()}>Try Again</button>
+        </div>
+      ) : (
+        <div className="question-card">
+          <h2>
+            Question: {currentQuestion + 1} out of {questions.length}
+          </h2>
+          <h3 className="question-text">{questions[currentQuestion].text}</h3>
+          <ul style={{ listStyle: "none" }}>
+            {questions[currentQuestion].options.map((option, index) => {
+              return (
+                <li
+                  key={option.id}
+                  onClick={() => optionClicked(option.isCorrect, index)}
+                  className={
+                    userAnswerIndex === index
+                      ? option.isCorrect
+                        ? "bg-success"
+                        : "bg-danger"
+                      : ""
+                  }
+                >
+                  <span className="option-number">
+                    {String.fromCharCode(65 + index)}.{" "}
+                  </span>
+                  {option.text}
+                </li>
+              );
+            })}
+          </ul>
+        </div>
+      )}
     </div>
   );
 }
